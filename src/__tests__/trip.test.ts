3cce6d9057090392a9991ceed9ca09455a3f90c8
Sumanth <sumanthrkumar@gmail.com>
2026-02-12 20:29:04 -0800

    Adding locations to trip tests

--- src/__tests__/trip.test.ts
+++ src/__tests__/trip.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect, beforeEach, afterAll } from 'vitest';
 import request from 'supertest';
 import app from '../index.js';
-import { before } from 'node:test';
 import { prisma } from '../lib/prisma.js';
 
 describe('Trip API Endpoints', () => {
@@ -31,8 +30,9 @@ describe('Trip API Endpoints', () => {
     });
 
     it('should return a 404 for retrieving a trip that does not exist', async () => {
+        const nonExistentId = "00000000-0000-0000-0000-000000000000";
         const get_response = await request(app)
-            .get('/trips/fakeID');
+            .get('/trips/' + nonExistentId);
 
         expect(get_response.status).toBe(404);
     });
@@ -45,16 +45,37 @@ describe('Trip API Endpoints', () => {
                 name: "Summer Vacation",
                 description: "My summer vacation",
                 startDate: "2026-06-01T10:00:00Z",
-                endDate: "2026-06-15T10:00:00Z"
+                endDate: "2026-06-15T10:00:00Z",
+                locations: [
+                    {
+                        "name": "Shinjuku",
+                        "latitude": 35.6929128,
+                        "longitude": 139.709008,
+                        "dayIndex": 1
+                    }, 
+                    {
+                        "name": "Shibuya",
+                        "latitude": 35.6619707,
+                        "longitude": 139.703795,
+                        "dayIndex": 1
+                    }
+                ]
             });
 
-        
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('id');
         expect(response.body.name).toBe("Summer Vacation")
         expect(response.body.description).toBe("My summer vacation")
         expect(response.body.startDate).toBe("2026-06-01T10:00:00.000Z")
         expect(response.body.endDate).toBe("2026-06-15T10:00:00.000Z")
+        expect(response.body.locations[0].name).toBe("Shinjuku")
+        expect(response.body.locations[0].latitude).toBe(35.6929128)
+        expect(response.body.locations[0].longitude).toBe(139.709008)
+        expect(response.body.locations[0].dayIndex).toBe(1)
+        expect(response.body.locations[1].name).toBe("Shibuya")
+        expect(response.body.locations[1].latitude).toBe(35.6619707)
+        expect(response.body.locations[1].longitude).toBe(139.703795)
+        expect(response.body.locations[1].dayIndex).toBe(1)
         const id = response.body.id;
 
         // Testing GET endpoint - retrieve a trip
@@ -68,7 +89,15 @@ describe('Trip API Endpoints', () => {
         expect(get_response.body.description).toBe("My summer vacation")
         expect(get_response.body.startDate).toBe("2026-06-01T10:00:00.000Z")
         expect(get_response.body.endDate).toBe("2026-06-15T10:00:00.000Z")
-
+        expect(get_response.body.locations.length).toBe(2);
+        expect(get_response.body.locations[0].name).toBe("Shinjuku")
+        expect(get_response.body.locations[0].latitude).toBe(35.6929128)
+        expect(get_response.body.locations[0].longitude).toBe(139.709008)
+        expect(get_response.body.locations[0].dayIndex).toBe(1)
+        expect(get_response.body.locations[1].name).toBe("Shibuya")
+        expect(get_response.body.locations[1].latitude).toBe(35.6619707)
+        expect(get_response.body.locations[1].longitude).toBe(139.703795)
+        expect(get_response.body.locations[1].dayIndex).toBe(1)
         
         // Testing POST endpoint - update a trip
         const update_response = await request(app)
@@ -89,10 +118,16 @@ describe('Trip API Endpoints', () => {
         expect(update_response.body.startDate).toBe("2026-11-01T10:00:00.000Z")
         expect(update_response.body.endDate).toBe("2026-11-15T10:00:00.000Z")
 
+        // No locations on this update as we didn't pass in a new locations array - existing locations SHOULD be deleted
+        expect(update_response.body.locations).toBeUndefined();
+
 
         // Testing DELETE endpoint - delete a trip
         const delete_response = await request(app).delete('/trips/' + id);
 
         expect(delete_response.status).toBe(200);
+
+        // Making sure there are no locations as trip was deleted
+        expect(await prisma.location.count()).toBe(0)
     });
 });
